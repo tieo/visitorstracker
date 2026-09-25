e33de47cb2cb8532adb33724871a3b628177f6c3
tieo <65707274+tieo@users.noreply.github.com>
2026-09-25 10:20:56 +0200

    Launcher label is the app's name, Behörden-Termin-Tracker Ulm/Ehingen; version 0.2.2

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "io.github.tieo.visitorstracker"
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
-        versionName = "0.2.1"
+        versionCode = 4
+        versionName = "0.2.2"
     }
 
     signingConfigs {
